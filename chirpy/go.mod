e3ab6d5672fda55e3f688de7a5cdd112599e6aca
Nathan <nategarcia.va@gmail.com>
2026-09-16 07:22:24 -0400

    Creates basic server and page.

---
+++ chirpy/go.mod
@@ -0,0 +1,3 @@
+module github.com/ngarcia610/bootdev/tree/main/chirpy
+
+go 1.26.8
---
+++ chirpy/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "net/http"
+)
+
+func main() {
+    mux := http.NewServeMux()
+
+    mux.Handle("/", http.FileServer(http.Dir(".")))
+
+    server := &http.Server{
+        Addr: ":8080",
+        Handler: mux,
+    }
+
+    server.ListenAndServe()
+}
