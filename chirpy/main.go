ee01ff12bbdc6b7e8c30f5a6da48fd177b787f3c
Nathan <nategarcia.va@gmail.com>
2026-09-16 21:58:16 -0400

    adds pages and health check

--- chirpy/main.go
+++ chirpy/main.go
@@ -7,7 +7,16 @@ import (
 func main() {
     mux := http.NewServeMux()
 
-    mux.Handle("/", http.FileServer(http.Dir(".")))
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+    })
+
+    mux.Handle(
+        "/app/",
+        http.StripPrefix("/app", http.FileServer(http.Dir("."))),
+    )
 
     server := &http.Server{
         Addr: ":8080",
